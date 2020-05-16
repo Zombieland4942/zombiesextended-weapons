d393be070380bcbe3e0043541a72ffba9fd89ddf
Zombieland4942 <zombieland4942@hotmail.com>
2020-05-16 10:25:48 +1000

    refactoring code

--- data.lua
+++ data.lua
@@ -1,11 +1,11 @@
 require("prototypes.configuration")
 require("prototypes.groups.groups")
 
-require("prototypes.rifles.assault-rifles")
+require("prototypes.weapons.assault-rifles")
 require("prototypes.ammo.assault-ammo")
 
-require("prototypes.rifles.sniper-rifles")
+require("prototypes.weapons.sniper-rifles")
 require("prototypes.ammo.sniper-ammo")
 
-require("prototypes.rifles.railgun-rifles")
+require("prototypes.weapons.railgun-rifles")
 require("prototypes.ammo.railgun-ammo")
